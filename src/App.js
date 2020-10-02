c37b3a6b6b639c2ddcdf43d7efe0d39f462788ee
anubhavvs <anubhavvsaha2001@gmail.com>
2020-10-02 19:20:03 +0530

    Added a FAQs Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
+import { FaqsContainer } from './containers/faqs';
 
 function App() {
   return (
     <>
     <JumbotronContainer />
+    <FaqsContainer />
     <FooterContainer />
     </>
   );
