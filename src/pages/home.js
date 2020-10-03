e45137ee7e265b53004e39ad2e814389e869fb55
anubhavvs <anubhavvsaha2001@gmail.com>
2020-10-03 16:50:32 +0530

    Added Home Page Header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { FooterContainer } from '../containers/footer'
+
+import { OptForm } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
 
 export default function Home() {
     return (
         <>
+            <HeaderContainer>
+            <OptForm>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                <OptForm.Break />
+                <OptForm.Input placeholder="Email address" />
+                <OptForm.Button>GET STARTED</OptForm.Button>
+            </OptForm>
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
