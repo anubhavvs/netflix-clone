e4a670a0511b62513bd9c5f23d120c3399a2cf0d
anubhavvs <anubhavvsaha2001@gmail.com>
2020-10-06 17:05:26 +0530

    Custom hook for Browse Page

---
+++ src/containers/browse.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function BrowserContainer() {
+    return(
+        <p>Browser Container!</p>
+    );
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useContent } from '../hooks';
+import { BrowserContainer } from '../containers/browse'
 
 export default function Browse() {
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+
     return (
         <p>Browse Page</p>
     )
