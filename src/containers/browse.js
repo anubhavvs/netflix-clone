50555ffc429af6d4d3ccb4a1506cd91856ba89ce
anubhavvs <anubhavvsaha2001@gmail.com>
2020-10-07 20:57:06 +0530

    Implemented Live Search in Browse Page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import { Loading, Header, Card, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
@@ -25,6 +26,18 @@ export function BrowseContainer({ slides }) {
     useEffect(() => {
         setSlideRows(slides[category]);
     }, [slides, category]);
+    
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
+        const results = fuse.search(searchTerm).map(({ item }) => item);
+    
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+          setSlideRows(results);
+        } else {
+          setSlideRows(slides[category]);
+        }
+        // eslint-disable-next-line
+    }, [searchTerm]);
 
     return profile.displayName ? (
         <>
